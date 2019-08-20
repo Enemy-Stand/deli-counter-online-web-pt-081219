6ad63734a690ddc4f76522bf27e81a48b8c20368
Joseph Sesti <sestijoey63@gmail.com>
2019-08-20 19:23:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ def line(array)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    deli.each_with_index
+    deli.each_with_index |person, i|
+    
   end
 end
 
