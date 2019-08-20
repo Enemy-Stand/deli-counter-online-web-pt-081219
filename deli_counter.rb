6fe7c117ae9a312e7aa16fb0df23f73f3f92eb0a
Joseph Sesti <sestijoey63@gmail.com>
2019-08-20 19:28:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(array)
   else
     current_line = "The line is currently:"
     deli.each.with_index do |person, i|
-      current_line(1) << "#{i}. #{person}"
+      current_line(1) << ("#{i}. #{person}")
     end
-    puts line
+    puts current_line
   end
 end
