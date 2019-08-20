57180154bb566f2472060fc52726e6f370727ab0
Joseph Sesti <sestijoey63@gmail.com>
2019-08-20 19:59:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name)
+def take_a_number(deli, name)
   deli << name
   puts "Welcome #{name}, you are number #{deli.length} in line."
   
