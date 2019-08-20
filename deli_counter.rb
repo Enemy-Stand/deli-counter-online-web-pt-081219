df991909314b570d3f161b0af87a9a8dbea3bcac
Joseph Sesti <sestijoey63@gmail.com>
2019-08-20 19:34:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_delideli)
   else
     current_line = "The line is currently:"
     katz_deli.each.with_index do |i, person|
-      current_line(1) << " #{i}. #{person}"
+      current_line << " #{i}. #{person}"
     end
     puts current_line
   end
