4ba67eeb893bab017deee449e98f6be1309d87d4
Joseph Sesti <sestijoey63@gmail.com>
2019-08-20 19:45:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     current_line = "The line is currently:"
-    katz_deli.each.with_index do |i, person|
-      current_line(1) << " #{i}. #{person}"
+    katz_deli.each.with_index(1) do |i, person|
+      current_line << " #{i}. #{person}"
     end
     puts current_line
   end
